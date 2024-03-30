7032487472538200d571816cc42dcbc66f9f8c4f
Do Duc Anh <doducanh2810@gmail.com>
2024-03-30 09:51:32 +0100

    Add split method

--- utils.c
+++ utils.c
@@ -4,6 +4,7 @@
 
 #include <string.h>
 #include "csapp.h"
+#include "utils.h"
 
 void send_message(int fd, char *message) {
     Rio_writen(fd, message, strlen(message));
@@ -11,4 +12,34 @@ void send_message(int fd, char *message) {
 
 int are_equal_strings(char *str1, char *str2) {
     return strcmp(str1, str2) == 0;
+}
+
+char **split_string_by_whitespace(const char* input_string, int* num_words) {
+    char** words = (char**)malloc(MAX_WORDS * sizeof(char*));
+    if (words == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    char* token;
+    char* copy = strdup(input_string);
+    if (copy == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    *num_words = 0;
+    token = strtok(copy, " \t\n");
+    while (token != NULL) {
+        words[*num_words] = strdup(token);
+        if (words[*num_words] == NULL) {
+            fprintf(stderr, "Memory allocation failed\n");
+            exit(1);
+        }
+        (*num_words)++;
+        token = strtok(NULL, " \t\n");
+    }
+
+    free(copy);
+    return words;
 }
--- utils.h
+++ utils.h
@@ -4,6 +4,7 @@
 #define PACKAGE_SIZE 8192
 #define SERVER_PORT 2121
 #define MAX_NAME_LEN 256
+#define MAX_WORDS 4096
 #define NB_PROC 1
 
 /* Send message to the connection using fd */
@@ -11,5 +12,7 @@ void send_message(int fd, char *message);
 
 /* Check if two strings are equal */
 int are_equal_strings(char *str1, char *str2);
+
+char **split_string_by_whitespace(const char* input_string, int* num_words);
 
 #endif
